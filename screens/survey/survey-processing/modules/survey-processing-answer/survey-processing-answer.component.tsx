ece81cd3d58a9cba5883a0394df79f93844b6faa
gimmizz <sdn1113@yonsei.ac.kr>
2022-06-27 01:52:25 +0900

    feat: answer api request 구현

--- screens/survey/survey-processing/modules/survey-processing-answer/survey-processing-answer.component.tsx
+++ screens/survey/survey-processing/modules/survey-processing-answer/survey-processing-answer.component.tsx
@@ -7,6 +7,7 @@ import * as data from './survey-processing-answer.data';
 type ISurveyProcessingAnswerComponentProp = {
   currentOrder: number;
   setCurrentOrder: any;
+  visitorId: number;
 };
 const HoverSpace = styled(Space)`
   :hover {
@@ -17,26 +18,29 @@ const HoverSpace = styled(Space)`
 export const SurveyProcessingAnswerComponent: FC<ISurveyProcessingAnswerComponentProp> = ({
   currentOrder,
   setCurrentOrder,
+  visitorId
 }) => {
-  const handleClickButton = (item: any) => {
-    setCurrentOrder(item.next_question_order);
-  };
-
   const saveUserAnswer = async (currentOrder: number, item: any) => {
-    // const response = {
-    //   visitor_id: "visitor_id", // visitor_id 를 어디서 구하냥..
-    //   question: currentOrder,
-    //   answer: item.order,
-    //   created_at: "yyyy-MM-dd HH:mm:ss"
-    // };
-    const response = await axios.post('http://localhost:8080/answer/club', {
-      visitor_id: "visitor_id", // visitor_id 를 어디서 구하냥..
+    const response = {
+      visitor_id: visitorId,
       question: currentOrder,
       answer: item.order,
       created_at: "yyyy-MM-dd HH:mm:ss"
-    });
+    };
+    // const response = await axios.post('http://localhost:8080/answer/club', {
+    //   visitor_id: {visitorId},
+    //   question: currentOrder,
+    //   answer: item.order,
+    //   created_at: "yyyy-MM-dd HH:mm:ss"
+    // });
     console.log(response);
   }
+
+  const handleClickButton = (item: any) => {
+    saveUserAnswer(currentOrder, item);
+    setCurrentOrder(item.next_question_order);
+  };
+
   const arrangeItemByRow = (currentOrder: number) => (
     <Row key={data.question_answer_data[currentOrder].length} gutter={[16, 24]}>
       {data.question_answer_data[currentOrder].map((item) => (
@@ -54,8 +58,7 @@ export const SurveyProcessingAnswerComponent: FC<ISurveyProcessingAnswerComponen
             padding: 10,
           }}
           onClick={() => {
-            saveUserAnswer(currentOrder, item);
-            handleClickButton(item); // 두 가지 동작이 실행되는데, 함수 이름이 너무 포괄적이진 않을까?
+            handleClickButton(item);
           }}
         >
           <div dangerouslySetInnerHTML={{ __html: item.option }} />
