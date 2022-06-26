4f57fda1237e0089945bd56787b06b88c5ae77cf
gimmizz <sdn1113@yonsei.ac.kr>
2022-06-27 01:51:35 +0900

    feat: visitor_id response useEffect로 구현

--- screens/survey/survey-home/modules/survey-home-detail/survey-home-detail.module.tsx
+++ screens/survey/survey-home/modules/survey-home-detail/survey-home-detail.module.tsx
@@ -2,7 +2,7 @@ import { LinkOutlined } from '@ant-design/icons';
 import { Button, Space } from 'antd';
 import axios from 'axios';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { getUserAgent } from './survey-home-detail.method';
 
@@ -15,37 +15,60 @@ const HoverButton = styled(Button)`
   }
 `;
 
-const saveVisitorInfo = async () => {
-  const userAgentResponse = await getUserAgent();
 
-  // const response = {
-  //   ip_address: "127.0.0.0",
-  //   agent_os: userAgentResponse.os,
-  //   agent_browser: userAgentResponse.browser,
-  //   access_url: "https://test",
-  //   created_at: "yyyy-MM-dd HH:mm:ss"
-  // };
-  const response = await axios.post('http://localhost:8080/start/club', {
-    ip_address: "127.0.0.0",
-    agent_os: userAgentResponse.os,
-    agent_browser: userAgentResponse.browser,
-    access_url: "https://test",
-    created_at: "yyyy-MM-dd HH:mm:ss"
-  });
-  console.log(response);
-}
+export const SurveyHomeDetailModule: FC<ISurveyHomeDetailModuleProps> = (hi: any) => {
+  const [visitorId, setVisitorId] = useState(0);
 
-export const SurveyHomeDetailModule: FC<ISurveyHomeDetailModuleProps> = () => {
+  const saveVisitorInfo = async () => {
+    const userAgentResponse = await getUserAgent();
+
+    // const response = await axios.post<any, { visitorId: number; }>('http://localhost:8080/start/club', {
+    //   ip_address: "127.0.0.0",
+    //   agent_os: userAgentResponse.os,
+    //   agent_browser: userAgentResponse.browser,
+    //   access_url: "https://test",
+    //   created_at: "yyyy-MM-dd HH:mm:ss"
+    // });
+
+    // console.log(response);
+    // setVisitorId(response.visitorId);
+
+
+    // 테스트 코드
+    const response = {
+      ip_address: "127.0.0.0",
+      agent_os: userAgentResponse.os,
+      agent_browser: userAgentResponse.browser,
+      access_url: "https://test",
+      created_at: "yyyy-MM-dd HH:mm:ss"
+    };
+
+    const expected_reponse = { visitorId: 12345678 } // visitor_id
+    console.log(expected_reponse);
+    setVisitorId(expected_reponse.visitorId);
+  }
+
+  useEffect(() => {
+    saveVisitorInfo();
+  }, []);
+
+  const handleStartButtonClick = () => {
+    saveVisitorInfo();
+  }
+
   return (
     <div style={{ display: 'flex', padding: 70, justifyContent: 'center' }}>
       <Space direction="vertical">
-        <Link href="/survey">
+        <Link href={{
+          pathname: "/survey",
+          query: { visitorId: visitorId }
+        }}>
           <a>
             <HoverButton
               type="primary"
               shape="round"
               size="large"
-              onClick={() => { saveVisitorInfo() }}
+              onClick={() => { handleStartButtonClick }}
             >
               지금 바로 시작하기
             </HoverButton>
