a6dbf4491a3ddbbdbd3bdc2a93c9640aed442619
gimmizz <sdn1113@yonsei.ac.kr>
2022-06-26 23:40:22 +0900

    refactor : survey-home-components and structure

--- screens/survey/survey-home/modules/survey-home-detail/survey-home-detail.module.tsx
+++ screens/survey/survey-home/modules/survey-home-detail/survey-home-detail.module.tsx
@@ -1,8 +1,10 @@
 import { LinkOutlined } from '@ant-design/icons';
 import { Button, Space } from 'antd';
+import axios from 'axios';
 import Link from 'next/link';
 import { FC } from 'react';
 import styled from 'styled-components';
+import { getUserAgent } from './survey-home-detail.method';
 
 type ISurveyHomeDetailModuleProps = {};
 
@@ -13,13 +15,38 @@ const HoverButton = styled(Button)`
   }
 `;
 
+const saveVisitorInfo = async () => {
+  const userAgentResponse = await getUserAgent();
+
+  // const response = {
+  //   ip_address: "127.0.0.0",
+  //   agent_os: userAgentResponse.os,
+  //   agent_browser: userAgentResponse.browser,
+  //   access_url: "https://test",
+  //   created_at: "yyyy-MM-dd HH:mm:ss"
+  // };
+  const response = await axios.post('http://localhost:8080/start/club', {
+    ip_address: "127.0.0.0",
+    agent_os: userAgentResponse.os,
+    agent_browser: userAgentResponse.browser,
+    access_url: "https://test",
+    created_at: "yyyy-MM-dd HH:mm:ss"
+  });
+  console.log(response);
+}
+
 export const SurveyHomeDetailModule: FC<ISurveyHomeDetailModuleProps> = () => {
   return (
     <div style={{ display: 'flex', padding: 70, justifyContent: 'center' }}>
       <Space direction="vertical">
         <Link href="/survey">
           <a>
-            <HoverButton type="primary" shape="round" size="large">
+            <HoverButton
+              type="primary"
+              shape="round"
+              size="large"
+              onClick={() => { saveVisitorInfo() }}
+            >
               지금 바로 시작하기
             </HoverButton>
           </a>
