d3b3ca93cb367a618b417915859964d3e0e2ce5f
gimmizz <sdn1113@yonsei.ac.kr>
2022-06-26 23:40:44 +0900

    answer api 구현 중

--- screens/survey/survey-processing/modules/survey-processing-answer/survey-processing-answer.component.tsx
+++ screens/survey/survey-processing/modules/survey-processing-answer/survey-processing-answer.component.tsx
@@ -1,4 +1,5 @@
 import { Row, Space } from 'antd';
+import axios from 'axios';
 import { FC } from 'react';
 import styled from 'styled-components';
 import * as data from './survey-processing-answer.data';
@@ -20,6 +21,22 @@ export const SurveyProcessingAnswerComponent: FC<ISurveyProcessingAnswerComponen
   const handleClickButton = (item: any) => {
     setCurrentOrder(item.next_question_order);
   };
+
+  const saveUserAnswer = async (currentOrder: number, item: any) => {
+    // const response = {
+    //   visitor_id: "visitor_id", // visitor_id 를 어디서 구하냥..
+    //   question: currentOrder,
+    //   answer: item.order,
+    //   created_at: "yyyy-MM-dd HH:mm:ss"
+    // };
+    const response = await axios.post('http://localhost:8080/answer/club', {
+      visitor_id: "visitor_id", // visitor_id 를 어디서 구하냥..
+      question: currentOrder,
+      answer: item.order,
+      created_at: "yyyy-MM-dd HH:mm:ss"
+    });
+    console.log(response);
+  }
   const arrangeItemByRow = (currentOrder: number) => (
     <Row key={data.question_answer_data[currentOrder].length} gutter={[16, 24]}>
       {data.question_answer_data[currentOrder].map((item) => (
@@ -37,7 +54,8 @@ export const SurveyProcessingAnswerComponent: FC<ISurveyProcessingAnswerComponen
             padding: 10,
           }}
           onClick={() => {
-            handleClickButton(item);
+            saveUserAnswer(currentOrder, item);
+            handleClickButton(item); // 두 가지 동작이 실행되는데, 함수 이름이 너무 포괄적이진 않을까?
           }}
         >
           <div dangerouslySetInnerHTML={{ __html: item.option }} />
